f885900abdd5e217a22ffb7975e127c672c1791c
gravityshouldbenaut <gravityshouldbenaut@Samarths-MacBook-Pro.local>
2015-05-02 16:47:34 -0700

    added form and post request w/o url

--- Qpost/ViewController.swift
+++ Qpost/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @IBOutlet var workImg: UIImageView!
+    var question:NSString!
+    @IBOutlet var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        question = textField.text
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func attachImage(sender: UIButton) {
+        workImg.image = UIImage(named: "mathsequations.jpg")
+    }
+    @IBAction func postToMYSQL(sender: AnyObject) {
+        var imageData = UIImagePNGRepresentation(UIImage(contentsOfFile: "mathsequations.jpg"))
+       
+        var request = NSMutableURLRequest(URL: NSURL(string: "http://www.google.com")!)
+        var session = NSURLSession.sharedSession()
+        request.HTTPMethod = "POST"
+//        var boundary:NSString = "0xKhTmLbOuNdArY"
+//         var kNewLine:NSString = "\r\n"
+//        
+        let body = NSMutableData()
+        
+        body.appendData((question as NSString).dataUsingEncoding(NSUTF8StringEncoding)!)
+        body.appendData(imageData)
+//        body.appendData((base64String as NSString).dataUsingEncoding(NSUTF8StringEncoding)!)
+        request.HTTPBody = body
+        
+    
+    
+    }
+    
 
 }
 
